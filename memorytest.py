45b2d7902f1dae5bd32e6d40450696ec714867f4
Gornoka <piupiumacher@gmail.com>
2020-03-13 17:22:37 +0100

    removed tracking of temp files

--- memorytest.py
+++ memorytest.py
@@ -1,4 +1,6 @@
 import time
+import tracemalloc
+
 import psutil
 from src import pytesseract
 import os
@@ -9,39 +11,46 @@ except ImportError:
 
 def to_pdf(images, remove=True,fname_out="1"):
     images[0].save(fname_out, format='tiff', append_images=images[1:], save_all=True, duration=500, loop=0)
-    pdf = pytesseract.image_to_pdf_or_hocr(fname_out[:-4] + '.tiff')
+    pdf = pytesseract.image_to_pdf_or_hocr(fname_out[:-5] + '.tiff')
     if remove:
         os.remove(fname_out)
-    with open(fname_out[:-4] + '.pdf', 'w+b') as f:
+    with open(fname_out[:-5] + '.pdf', 'w+b') as f:
         f.write(bytearray(pdf))
 
-def ensure_directory():
-    if not os.path.exists(temppath):
-        os.mkdir(temppath)
+def ensure_directory(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
 
 if __name__ == '__main__':
-    number_images = 1000
+    tracemalloc.start()
+    number_images = 100
     temppath = 'temp_pdf_path'
     images = []
     print("current memory usage pre image loading : ")
     print(psutil.virtual_memory())
+    print(tracemalloc.get_traced_memory())
     for _ in range(number_images):
-        images.append(Image.open('tests/data/test.png'))
+        images.append(Image.open('tests/data/test.tiff'))
     print("current memory usage after image loading : ")
     print(psutil.virtual_memory())
+    print(tracemalloc.get_traced_memory())
     time.sleep(5)
     print("current memory usage after waiting : ")
     print(psutil.virtual_memory())
-
-    for i,im in enumerate(images):
+    print(tracemalloc.get_traced_memory())
+    ensure_directory(temppath)
+    for i, im in enumerate(images):
         print("current memory usage before image {} : ".format(i))
         print(psutil.virtual_memory())
-        to_pdf(temppath, i)
+        print(tracemalloc.get_traced_memory())
+        to_pdf(images, fname_out=temppath+'/'+str(i)+".tiff")
     print("current memory usage after all images written : ")
     print(psutil.virtual_memory())
+    print(tracemalloc.get_traced_memory())
     time.sleep(15)
     print("current memory usage after waiting : ")
     print(psutil.virtual_memory())
+    print(tracemalloc.get_traced_memory())
 
 
 
